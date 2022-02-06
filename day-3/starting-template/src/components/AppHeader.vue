ad7c64a3318be428d0d4c305d67e72795ace0174
Nikolay Fedorov <kofe88@mail.ru>
2022-02-06 04:43:58 +0300

    3 homework

---
+++ day-3/starting-template/src/components/AppHeader.vue
@@ -0,0 +1,38 @@
+<template>
+       <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a href="#" class="navbar-brand">Marvel</a>
+
+          
+          <form class="d-flex">
+            <input class="form-control me-2" 
+            type="search" 
+            placeholder="Поиск" 
+            aria-label="Search"
+            v-model="search"
+            @input="changeSearch(search)"
+            >
+            <button class="btn btn-outline-light" 
+            type="reset"
+            @click="()=>{search=''; changeSearch(search)}">
+            Все</button>
+          </form>
+        </div>
+      </nav>
+</template>
+
+<script>
+    export default {
+        name: "AppHeader",
+        props: ['changeSearch'],
+        data() {
+            return {
+              search: '',
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
