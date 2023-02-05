4e76e2350cfeed9c37c9fffa8f3023fd71ab009a
Nikolay Fedorov <kofe88@mail.ru>
2023-02-05 16:01:36 +0300

    new vue version

--- day-3/result/vue.config.js
+++ day-3/result/vue.config.js
@@ -2,5 +2,22 @@ module.exports = {
   pwa: {
     name: 'marvel',
     themeColor: '#212121'
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias.set('vue', '@vue/compat')
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2
+            }
+          }
+        }
+      })
   }
 }
